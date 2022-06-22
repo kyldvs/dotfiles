6b6a5d52f1b5af4484fc940951f8a685044ca084
Kyle Davis <kad@fb.com>
2022-06-22 15:36:31 -0700

    Add more methods to config impl

--- src/config.rs
+++ src/config.rs
@@ -49,6 +49,75 @@ struct PathBase {
   dirs: Option<Array>,
 }
 
+// Boilerplate for all the platforms.
+
+trait Platformed<T> {
+  fn arch(&self) -> &Option<T>;
+  fn mac(&self) -> &Option<T>;
+  fn windows(&self) -> &Option<T>;
+  fn get_platformed(&self) -> &Option<T> {
+    let res = if platform::is_mac() {
+      self.mac()
+    } else if platform::is_arch_linux() {
+      self.arch()
+    } else if platform::is_windows() {
+      self.windows()
+    } else {
+      &None
+    };
+    res
+  }
+}
+
+impl Platformed<PathBase> for Path {
+  fn mac(&self) -> &Option<PathBase> {
+    &self.mac
+  }
+  fn arch(&self) -> &Option<PathBase> {
+    &self.arch
+  }
+  fn windows(&self) -> &Option<PathBase> {
+    &self.windows
+  }
+}
+
+// Implementations for each step's struct.
+
+impl Path {
+  fn dirs(&self) -> Vec<String> {
+    let mut result: Vec<String> = vec![];
+
+    match &self.dirs {
+      | None => (),
+      | Some(dirs) => {
+        for dir in dirs {
+          if dir.is_str() {
+            result.push(String::from(dir.as_str().unwrap()));
+          }
+        }
+      },
+    };
+
+    match &self.get_platformed() {
+      | None => (),
+      | Some(base) => match &base.dirs {
+        | None => (),
+        | Some(dirs) => {
+          for dir in dirs {
+            if dir.is_str() {
+              result.push(String::from(dir.as_str().unwrap()));
+            }
+          }
+        },
+      },
+    };
+
+    result
+  }
+}
+
+// Overall Implementation.
+
 impl Config {
   pub fn from_string(s: &String) -> Config {
     // TODO: Error handline when parsing toml.
@@ -63,46 +132,14 @@ impl Config {
   }
 
   pub fn get_path_dirs(&self) -> Vec<String> {
-    let config = &self;
-    let step = config.step.as_ref();
-    let path = step.and_then(|x| x.path.as_ref());
-
-    // This is awful, what is the idiomatic way to "Chain" options?
-    let mac = path.and_then(|x| x.mac.as_ref());
-    let arch = path.and_then(|x| x.arch.as_ref());
-    let windows = path.and_then(|x| x.windows.as_ref());
-    let platform = if platform::is_mac() {
-      mac
-    } else if platform::is_arch_linux() {
-      arch
-    } else if platform::is_windows() {
-      windows
-    } else {
-      None
-    };
-
     let empty = vec![];
-
-    let dirs = path.and_then(|x| x.dirs.as_ref());
-    let dirs = dirs.unwrap_or(&empty);
-
-    let pdirs = platform.and_then(|x| x.dirs.as_ref());
-    let pdirs = pdirs.unwrap_or(&empty);
-
-    let mut result: Vec<String> = Vec::new();
-    for dir in dirs {
-      if dir.is_str() {
-        let s = String::from(dir.as_str().unwrap());
-        result.push(s);
-      }
-    }
-
-    for dir in pdirs {
-      if dir.is_str() {
-        let s = String::from(dir.as_str().unwrap());
-        result.push(s);
-      }
-    }
-    return result;
+    let dirs = match &self.step {
+      | None => empty,
+      | Some(step) => match &step.path {
+        | None => empty,
+        | Some(path) => path.dirs(),
+      },
+    };
+    return dirs;
   }
 }
