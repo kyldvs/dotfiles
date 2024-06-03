a26ae1fd65c39f2fe12c4b2f2f2d179ec910fe06
Kyle <github@kyle.dev>
2024-06-03 10:29:41 -0700

    feat: maximize wezterm on open

--- links/wezterm/.config/wezterm/wezterm.lua
+++ links/wezterm/.config/wezterm/wezterm.lua
@@ -1,13 +1,18 @@
 local wezterm = require 'wezterm'
 local mux = wezterm.mux
 
--- Kind of works to maximize the window, but it's slow on mac. Using the
--- rectangle window manager to do this manually for now. For more discussion
--- see: https://github.com/wez/wezterm/issues/3299
--- wezterm.on("gui-startup", function(cmd)
--- 	local tab, pane, window = mux.spawn_window(cmd or {})
--- 	window:gui_window():maximize()
--- end)
+wezterm.on("gui-startup", function(cmd)
+  local active = wezterm.gui.screens().active
+  local tab, pane, window = mux.spawn_window(cmd or {
+    x = active.x,
+    y = active.y,
+    width = active.width,
+    height = active.height,
+  })
+  -- I don't think this is needed, but also set the positions after spawn.
+  window:gui_window():set_position(active.x, active.y)
+  window:gui_window():set_inner_size(active.width, active.height)
+end)
 
 return {
 	color_scheme = 'Catppuccin Mocha',
