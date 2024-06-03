a9603e696cf2149bdaa2fc0cd4d69225f6d1d475
Kyle <github@kyle.dev>
2024-06-03 11:31:39 -0700

    feat: initial zsh setup

---
+++ links/zsh/.vcs.zsh
@@ -0,0 +1,263 @@
+#!/bin/zsh
+
+### My old commands for version control, will update __eventually__ ###
+
+# Function to print an error message.
+alias print_err=">&2 echo Error: "
+
+# Gets a string representation for the kind of repository we are in.
+#
+#   Git repos print:       "git"
+#   Mercurial repos print: "hg"
+#   Anything else prints:  "none"
+#
+function vcs_kind() {
+  IS_IN_GIT=$(git rev-parse --git-dir 2> /dev/null)
+  if [ $IS_IN_GIT ]; then
+    echo "git"
+    return 1
+  fi
+  IS_IN_MERCURIAL=$(hg root 2> /dev/null)
+  if [ $IS_IN_MERCURIAL ]; then
+    echo "hg"
+    return 1
+  fi
+  echo "none"
+}
+
+# Prints a generic error message for an unknown vcs then exits.
+#
+# Note: Always return 1 after calling this function so the method will break
+# logical chains properly. (TODO: Can this be done automatically somehow?)
+function vcs_none() {
+  print_err "$(pwd) is not managed by a known version control system"
+  return 1
+}
+
+# Function to check if in a mercurial repository.
+#
+# Arugments are ignored.
+function is_hg() {
+  case $(vcs_kind) in
+    git)
+      print_err "This is a git repository, expected mercurial."
+      return 1
+      ;;
+    hg)
+      return 0
+      ;;
+    *)
+      vcs_none
+      return 1
+      ;;
+  esac
+}
+
+# Create a new commit with the given message.
+#
+# Accepts a single string argument that is the message.
+function cm() {
+  if [ -z "$1" ]; then
+    print_err "Must provide a single message argument, no arguments provided."
+    return 1
+  fi
+
+  if [ -n "$2" ]; then
+    print_err "Must provide a single message argument, too many arguments provided."
+    return 1
+  fi
+
+  case $(vcs_kind) in
+    git)
+      # Have to git add first in order to capture untracked files.
+      git add -A && git commit -am "$1"
+      return 0
+      ;;
+    hg)
+      hg commit -Am "$1"
+      return 0
+      ;;
+    *)
+      vcs_none
+      return 1
+      ;;
+  esac
+}
+
+# Ammends the commit with the current changes.
+#
+# Accepts no arguments.
+function ca() {
+  if [ -n "$1" ]; then
+    print_err "There should be no arguments, too many arguments provided."
+    return 1
+  fi
+
+  case $(vcs_kind) in
+    git)
+      git add -A && git commit --amend --no-edit
+      return 0
+      ;;
+    hg)
+      hg commit --amend --rebase -Am "`hg log -l 1 --template "{desc}"`"
+      return 0
+      ;;
+    *)
+      vcs_none
+      return 1
+      ;;
+  esac
+}
+
+# Prints the current status of the repository.
+#
+# Accepts no arguments.
+function st() {
+  if [ -n "$1" ]; then
+    print_err "There should be no arguments, too many arguments provided."
+    return 1
+  fi
+
+  case $(vcs_kind) in
+    git)
+      git status
+      return 0
+      ;;
+    hg)
+      hg status
+      return 0
+      ;;
+    *)
+      vcs_none
+      return 1
+      ;;
+  esac
+}
+
+# Prints a graph of the current repository's commit structure. Only printing
+# the current path to master if possible.
+#
+# Accepts no arguments.
+function sl() {
+  if [ -n "$1" ]; then
+    print_err "There should be no arguments, too many arguments provided."
+    return 1
+  fi
+
+  case $(vcs_kind) in
+    git)
+      git log \
+      -20 \
+      --graph \
+      --abbrev-commit \
+      --decorate \
+      --format=format:'%C(bold blue)%h%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an%C(reset)'
+      return 0
+      ;;
+    hg)
+      hg --config extensions.pager=! ssl --all -r top%master
+      return 0
+      ;;
+    *)
+      vcs_none
+      return 1
+      ;;
+  esac
+}
+
+# Prints a graph of the current repository's commit structure. Prints all
+# commits.
+#
+# Accepts no arguments.
+function sll() {
+  if [ -n "$1" ]; then
+    print_err "There should be no arguments, too many arguments provided."
+    return 1
+  fi
+
+  case $(vcs_kind) in
+    git)
+      # Git has no special handling here, reuse sl.
+      sl
+      return 0
+      ;;
+    hg)
+      hg --config ui.paginate=never ssl
+      return 0
+      ;;
+    *)
+      vcs_none
+      return 1
+      ;;
+  esac
+}
+
+# Go to a particular place in the repository.
+#
+# Passes all arguments to underlying command.
+function go() {
+  case $(vcs_kind) in
+    git)
+      git checkout "$@"
+      return 0
+      ;;
+    hg)
+      hg checkout "$@"
+      return 0
+      ;;
+    *)
+      vcs_none
+      return 1
+      ;;
+  esac
+}
+
+# Gets the current commit hash in the repository.
+#
+# Accepts no arguments.
+function curr_hash() {
+  if [ -n "$1" ]; then
+    print_err "There should be no arguments, too many arguments provided."
+    return 1
+  fi
+
+  case $(vcs_kind) in
+    git)
+      git log -1 --pretty=format:"%h"
+      return 0
+      ;;
+    hg)
+      hg log -l 1 --template "{node|short}"
+      return 0
+      ;;
+    *)
+      vcs_none
+      return 1
+      ;;
+  esac
+}
+
+# Submits a diff. Only works in mercurial.
+#
+# If no arguments are specified it submits the currently active commit,
+# otherwise all arguments are passed to the underlying command.
+function s() {
+  case $(vcs_kind) in
+    git)
+      print_err "Cannot submit diffs from git repos, only www."
+      return 1
+      ;;
+    hg)
+      if [[ -z "$1" ]]; then
+        jf s --prepare $(curr_hash)
+      else
+        jf s --prepare "$@"
+      fi
+      return 0
+      ;;
+    *)
+      vcs_none
+      return 1
+      ;;
+  esac
+}
---
+++ links/zsh/.zshrc
@@ -0,0 +1,139 @@
+# =============================================================================
+# Homebrew (package manager).
+# =============================================================================
+
+if [[ -f "/opt/homebrew/bin/brew" ]] then
+  # If you're using macOS, you'll want this enabled.
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+# =============================================================================
+# zinit (zsh plugin manager).
+# =============================================================================
+
+# Set the directory to store zinit and plugins.
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+
+# Download Zinit, if it's not there yet.
+if [ ! -d "$ZINIT_HOME" ]; then
+  mkdir -p "$(dirname $ZINIT_HOME)"
+  git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+fi
+
+# Load zinit.
+source "${ZINIT_HOME}/zinit.zsh"
+
+# Add in Powerlevel10k.
+zinit ice depth=1; zinit light romkatv/powerlevel10k
+
+# Add in zsh plugins.
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-autosuggestions
+zinit light Aloxaf/fzf-tab
+
+# Add in snippets.
+# zinit snippet OMZP::git
+# zinit snippet OMZP::sudo
+# zinit snippet OMZP::archlinux
+# zinit snippet OMZP::aws
+# zinit snippet OMZP::kubectl
+# zinit snippet OMZP::kubectx
+# zinit snippet OMZP::command-not-found
+
+# =============================================================================
+# Completions.
+# =============================================================================
+
+# Load & run completions.
+autoload -Uz compinit && compinit
+
+# Make sure hidden folders and files can be completed (ones that start with .).
+_comp_options+=(globdots)
+
+# Replay completion definitions (to be done after compinit).
+zinit cdreplay -q
+
+# =============================================================================
+# Misc options.
+# =============================================================================
+
+# No beeping please.
+setopt NO_BEEP
+
+# =============================================================================
+# p10k (powerline).
+# =============================================================================
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f "~/.p10k.zsh" ]] || source "~/.p10k.zsh"
+
+# =============================================================================
+# nvm (nodejs version manager).
+# =============================================================================
+
+export NVM_DIR="$HOME/.nvm"
+# This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# This loads nvm bash_completion
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# =============================================================================
+# History.
+# =============================================================================
+
+HISTSIZE=5000
+HISTFILE=~/.zsh_history
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
+
+# =============================================================================
+# Completion styling.
+# =============================================================================
+
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
+# =============================================================================
+# Aliases.
+# =============================================================================
+
+# Allows easily attaching to existing tmux session.
+alias tt="(tmux ls | grep -vx attached && tmux at) || tmux"
+
+# =============================================================================
+# Source other zsh files.
+# =============================================================================
+
+[[ ! -f "~/.vcs.zsh" ]] || source "~/.vcs.zsh"
+
+# =============================================================================
+# pnpm (nodejs package manager).
+# =============================================================================
+
+# pnpm
+export PNPM_HOME="/Users/kad/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm endexport
+
+# =============================================================================
+# Shell integrations.
+# =============================================================================
+
+eval "$(fzf --zsh)"
+
+# Note: This will replace the "cd" command with zoxide.
+eval "$(zoxide init --cmd cd zsh)"
